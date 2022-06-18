450f6fa2f830957d74556c70d81c5c8a6278c104
pavveu <pavveu7@gmail.com>
2022-06-18 19:50:52 +0200

    add normal triangle vector utility function

--- src/utility.h
+++ src/utility.h
@@ -1,6 +1,7 @@
 #pragma once 
 
 #include <vector>
+#include <glm/glm.hpp>
 
 // Initialize empty matrix made of std::vector 
 // Example usage: (initialize 5x4 matrix of one's)
@@ -11,4 +12,12 @@ void initializeMatrix(std::vector<std::vector<T>>& matrix, int rows, int cols, T
     assert (matrix.empty());
     for (int i = 0; i < rows; i++)
         matrix.emplace_back(cols, val);
+}
+
+// Calculate Normal vector on Triangle A-B-C using vectors A->B and A->C
+// Resulting Vector is a vector starting in A vertex 
+// Assume: A, B,C are given in counter-clockwise order
+glm::vec3 calculateTriangleNormal(glm::vec3 A, glm::vec3 B, glm::vec3 C) {
+    glm::vec3 crossProduct = glm::cross(B-A, C-A);
+    return normalize(crossProduct);
 }
