89021f181a3daaa79f971ac056c198fdf6571578
Ignacy Stępka <ignacy.stepka@gmail.com>
2022-06-28 20:23:13 +0200

    Update animation shader

--- bin/v_animated.glsl
+++ bin/v_animated.glsl
@@ -3,8 +3,8 @@
 layout(location = 0) in vec3 pos;
 layout(location = 1) in vec3 norm;
 layout(location = 2) in vec2 tex;
-layout(location = 5) in ivec4 boneIds; 
-layout(location = 6) in vec4 weights;
+layout(location = 3) in ivec4 boneIds; 
+layout(location = 4) in vec4 weights;
 	
 uniform mat4 P;
 uniform mat4 V;
@@ -18,24 +18,24 @@ out vec2 texCoords;
 	
 void main()
 {
-    // vec4 totalPosition = vec4(0.0f);
-    // for(int i = 0 ; i < MAX_BONE_INFLUENCE ; i++)
-    // {
-    //     if(boneIds[i] == -1) 
-    //         continue;
-    //     if(boneIds[i] >=MAX_BONES) 
-    //     {
-    //         totalPosition = vec4(pos,1.0f);
-    //         break;
-    //     }
-    //     vec4 localPosition = finalBonesMatrices[boneIds[i]] * vec4(pos,1.0f);
-    //     totalPosition += localPosition * weights[i];
-    //     vec3 localNormal = mat3(finalBonesMatrices[boneIds[i]]) * norm;
-    // }
+    vec4 totalPosition = vec4(0.0f);
+    for(int i = 0 ; i < MAX_BONE_INFLUENCE ; i++)
+    {
+        if(boneIds[i] == -1) 
+            continue;
+        if(boneIds[i] >=MAX_BONES) 
+        {
+            totalPosition = vec4(pos,1.0f);
+            break;
+        }
+        vec4 localPosition = finalBonesMatrices[boneIds[i]] * vec4(pos,1.0f);
+        totalPosition += localPosition * weights[i];
+        vec3 localNormal = mat3(finalBonesMatrices[boneIds[i]]) * norm;
+    }
 		
-    // mat4 viewModel = V * M;
-    // gl_Position =  P * viewModel * totalPosition;
+    mat4 viewModel = V * M;
+    gl_Position =  P * viewModel * totalPosition;
 
-    gl_Position = P * V * M * vec4(pos,1.0f); // temporary
+   // gl_Position = P * V * M * vec4(pos,1.0f); // temporary
     texCoords = tex;
 }
