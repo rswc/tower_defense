c9b76f57c9707404362e6db80e019a1f18c0ad79
pavveu <pavveu7@gmail.com>
2022-06-30 02:27:18 +0200

    add core of Tower class

--- src/tower.cpp
+++ src/tower.cpp
@@ -1,13 +1,64 @@
 #include "tower.h"
 
+#include "resources.h"
+#include "shaderprogram.h"
+
+#include <glm/glm.hpp>
+#include <glm/gtc/type_ptr.hpp>
+#include <glm/gtc/matrix_transform.hpp>
+
 #include <iostream>
 
-void Tower::Update(double deltaTime) {
-    if (init)
-    {
-        init = false;
-        std::cout << "Tower is alive!" << std::endl;
-    }
-    
+Tower::Tower(BulletManager * bman, MobManager * mman) {
+    bulletman = bman;
+    mobman = mman;
+
+    tex = Resources::GetModelTexture(Resources::TOWER_TEXTURE);
+    mesh = Resources::GetAssimpModelMesh(Resources::TOWER_MODEL, 0);
+
+    Rotate(-AI_MATH_PI/2, glm::vec3(1.0f, 0.0f, 0.0f));
+    // Rotate(30.0f, glm::vec3(1.0f, 0.0f, 0.0f));
+    SetScale(glm::vec3(0.2, 0.2f, 0.2f));
 }
 
+void Tower::Draw(const Camera& camera) const {
+    if (!active) return;
+
+    //Activate the shader
+    spLambertTextured->use();
+
+    //TODO: move to Material class??
+	glUniformMatrix4fv(spLambertTextured->u("P"), 1, false, glm::value_ptr(camera.GetP()));
+	glUniformMatrix4fv(spLambertTextured->u("V"), 1, false, glm::value_ptr(camera.GetV()));
+	glUniformMatrix4fv(spLambertTextured->u("M"), 1, false, glm::value_ptr(GetTransformMatrix()));
+
+
+    //TODO: pass to Material class?? make separate Mesh class for holding attributes??
+	glEnableVertexAttribArray(spLambertTextured->a("vertex"));
+	glVertexAttribPointer(spLambertTextured->a("vertex"), 4, GL_FLOAT, false, 0, mesh->vertices.data()); 
+
+        
+	glEnableVertexAttribArray(spLambertTextured->a("texCoord"));
+	glVertexAttribPointer(spLambertTextured->a("texCoord"), 2, GL_FLOAT, false, 0, mesh->textures.data()); 
+
+       
+	glEnableVertexAttribArray(spLambertTextured->a("normal"));
+	glVertexAttribPointer(spLambertTextured->a("normal"), 4, GL_FLOAT, false, 0, mesh->normals.data()); 
+
+
+    glActiveTexture(GL_TEXTURE0);
+    glBindTexture(GL_TEXTURE_2D, tex);
+    glUniform1i(spLambertTextured->u("tex"), 0);
+
+    // glActiveTexture(GL_TEXTURE1);
+    // glBindTexture(GL_TEXTURE_2D, textSpecular);
+    // glUniform1i(spTexturedSpecular->u("textureMap1"), 1);
+
+    glDrawElements(GL_TRIANGLES, mesh->indices.size(), GL_UNSIGNED_INT, mesh->indices.data());
+
+    //Disable vertex attribute array
+	glDisableVertexAttribArray(spLambertTextured->a("vertex"));
+	glDisableVertexAttribArray(spLambertTextured->a("texCoord"));
+	glDisableVertexAttribArray(spLambertTextured->a("normal"));
+
+}
--- src/tower.h
+++ src/tower.h
@@ -1,15 +1,79 @@
 #pragma once
 
 #include "gameobject.h"
+#include "mobmanager.h"
+#include "BaseMesh.h"
 
-class Tower : public GameObject
-{
+class BulletManager;
+
+class Tower : public GameObject {
+    friend class TowerManager;
 private:
-    // mesh...
-    bool init = true;
+    
+    static constexpr float EPS = 1e-9;
+    static constexpr float refreshTime = 5.0f;
+    static constexpr float shootTime = 1.0f;
+    static constexpr float radius = 10.0f;
+    static constexpr float towerHeight = 1.0f;
+
+    float timeLeft = 0;
+    bool active = false;
+    bool ready = false;
+
+    MobManager * mobman = nullptr;
+    BulletManager * bulletman = nullptr;
+    BaseMesh *mesh = nullptr;
+    GLuint tex;
+
 public:
+    Tower(BulletManager * bman, MobManager * mman);
+    
+    void restart() {
+        timeLeft = refreshTime;
+        ready = false;
+        active = true;
+    }
 
-    void Draw(const Camera& camera) const override {};
-    void Update(double deltaTime) override;
+    inline void deactivate() { active = false; } 
+    
+    glm::vec3 getShootPoint() const {
+        return GetPosition() + glm::vec3(0.0f, towerHeight, 0.0f);
+    }
+    
+    inline bool inRange(glm::vec3 position) {
+        glm::vec3 diff = position - getShootPoint();
+        return diff.length() < radius;
+    }
+    
+    void shoot() {
+        if (!ready) return;
+
+        for (MobObject * mob : mobman->mobs) {
+            if (!mob->isActive()) continue;
+
+            std::cout << "Checking mob with id: " << mob->id() << std::endl;
+            if (!mob->shootable(shootTime)) { 
+                std::cout << "Mob is not shootable..." << std::endl; 
+                continue;
+            }
+            if (!inRange(mob->getModelHitCoordinates(shootTime))) { 
+                std::cout << "Mob not in range.." << std::endl;
+                continue;
+            }
+            std::cout << "Shooting that naggers " << mob->id() << std::endl;
+            mob->markDead(shootTime);
+            timeLeft = refreshTime;
+            ready = false;
+            return;
+        }
+    }
+
+    void Draw(const Camera& camera) const override;
+
+    void Update(double deltaTime)  override {
+        if (!active) return;
+
+        if (!ready && timeLeft >= 0) { timeLeft -= deltaTime; }
+        else { ready = true; shoot(); }
+    }
 };
-
