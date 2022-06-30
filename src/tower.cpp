290e46418c2366eb999e5e8ad5a1ed2368cdfd7a
rswc <rshoryti@protonmail.com>
2022-06-30 11:27:30 +0200

    Add lighting to towers

--- src/tower.cpp
+++ src/tower.cpp
@@ -55,30 +55,51 @@ void Tower::Draw(const Camera& camera) const {
     if (!active) return;
 
     //Activate the shader
-    spLambertTextured->use();
+    spTextured->use();
 
     //TODO: move to Material class??
-	glUniformMatrix4fv(spLambertTextured->u("P"), 1, false, glm::value_ptr(camera.GetP()));
-	glUniformMatrix4fv(spLambertTextured->u("V"), 1, false, glm::value_ptr(camera.GetV()));
-	glUniformMatrix4fv(spLambertTextured->u("M"), 1, false, glm::value_ptr(GetTransformMatrix()));
+	glUniformMatrix4fv(spTextured->u("P"), 1, false, glm::value_ptr(camera.GetP()));
+	glUniformMatrix4fv(spTextured->u("V"), 1, false, glm::value_ptr(camera.GetV()));
+	glUniformMatrix4fv(spTextured->u("M"), 1, false, glm::value_ptr(GetTransformMatrix()));
+
+    glUniform3fv(spTextured->u("cameraPos"), 1, glm::value_ptr(camera.GetPosition()));
+
+    glUniform3f(spTextured->u("dirAmbientColor"), 0.2f, 0.2f, 0.2f);
+	glUniform3f(spTextured->u("dirDiffuseColor"), 0.5f, 0.5f, 0.5f);
+	glUniform4f(spTextured->u("lightDir"), 0.2f, -1.0f, 0.0f, 0.0f);
+
+    auto pLights = camera.GetLights();
+    for (int i = 0; i < pLights.size(); ++i) {
+        std::string attrPrefix = "lights[" + std::to_string(i) + "].";
+
+        auto& pLight = pLights[i];
+
+        glUniform3fv(spTextured->u((attrPrefix + "position").c_str()), 1, glm::value_ptr(pLight.position));
+        glUniform3fv(spTextured->u((attrPrefix + "ambient").c_str()), 1, glm::value_ptr(pLight.ambient));
+        glUniform3fv(spTextured->u((attrPrefix + "diffuse").c_str()), 1, glm::value_ptr(pLight.diffuse));
+        glUniform3fv(spTextured->u((attrPrefix + "specular").c_str()), 1, glm::value_ptr(pLight.specular));
+        glUniform1f(spTextured->u((attrPrefix + "A").c_str()), pLight.A);
+        glUniform1f(spTextured->u((attrPrefix + "B").c_str()), pLight.B);
+        glUniform1f(spTextured->u((attrPrefix + "C").c_str()), pLight.C);
+    }
 
 
     //TODO: pass to Material class?? make separate Mesh class for holding attributes??
-	glEnableVertexAttribArray(spLambertTextured->a("vertex"));
-	glVertexAttribPointer(spLambertTextured->a("vertex"), 4, GL_FLOAT, false, 0, mesh->vertices.data()); 
+	glEnableVertexAttribArray(spTextured->a("vertex"));
+	glVertexAttribPointer(spTextured->a("vertex"), 4, GL_FLOAT, false, 0, mesh->vertices.data()); 
 
         
-	glEnableVertexAttribArray(spLambertTextured->a("texCoord"));
-	glVertexAttribPointer(spLambertTextured->a("texCoord"), 2, GL_FLOAT, false, 0, mesh->textures.data()); 
+	glEnableVertexAttribArray(spTextured->a("texCoord"));
+	glVertexAttribPointer(spTextured->a("texCoord"), 2, GL_FLOAT, false, 0, mesh->textures.data()); 
 
        
-	glEnableVertexAttribArray(spLambertTextured->a("normal"));
-	glVertexAttribPointer(spLambertTextured->a("normal"), 4, GL_FLOAT, false, 0, mesh->normals.data()); 
+	glEnableVertexAttribArray(spTextured->a("normal"));
+	glVertexAttribPointer(spTextured->a("normal"), 4, GL_FLOAT, false, 0, mesh->normals.data()); 
 
 
     glActiveTexture(GL_TEXTURE0);
     glBindTexture(GL_TEXTURE_2D, tex);
-    glUniform1i(spLambertTextured->u("tex"), 0);
+    glUniform1i(spTextured->u("tex"), 0);
 
     // glActiveTexture(GL_TEXTURE1);
     // glBindTexture(GL_TEXTURE_2D, textSpecular);
@@ -87,8 +108,8 @@ void Tower::Draw(const Camera& camera) const {
     glDrawElements(GL_TRIANGLES, mesh->indices.size(), GL_UNSIGNED_INT, mesh->indices.data());
 
     //Disable vertex attribute array
-	glDisableVertexAttribArray(spLambertTextured->a("vertex"));
-	glDisableVertexAttribArray(spLambertTextured->a("texCoord"));
-	glDisableVertexAttribArray(spLambertTextured->a("normal"));
+	glDisableVertexAttribArray(spTextured->a("vertex"));
+	glDisableVertexAttribArray(spTextured->a("texCoord"));
+	glDisableVertexAttribArray(spTextured->a("normal"));
 
 }
